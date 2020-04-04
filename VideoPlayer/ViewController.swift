4e58a30ab28d05b754c26000c6dd49d3dd4e82a1
Afraz Siddiqui <afsiddiq@microsoft.com>
2020-04-04 15:03:03 -0400

    Play Video demo

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Afraz Siddiqui on 4/4/20.
+//  Copyright © 2020 ASN GROUP LLC. All rights reserved.
+//
+
+import AVKit
+import AVFoundation
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        let player = AVPlayer(url: URL(fileURLWithPath: Bundle.main.path(forResource: "train", ofType: "m4v")!))
+        let layer = AVPlayerLayer(player: player)
+        layer.frame = view.bounds
+        layer.videoGravity = .resizeAspectFill
+        view.layer.addSublayer(layer)
+        player.volume = 0
+        player.play()
+
+//        let vc = AVPlayerViewController()
+//        vc.player = player
+//        present(vc, animated: true)
+    }
+
+
+}
+
